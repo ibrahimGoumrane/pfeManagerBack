a01be266454c6f28f89c8ad0ce2f93b173304f10
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2025-05-20 23:29:47 +0100

    Update .env.example for database configuration and add development mode settings; include DigitalOcean filesystem configuration and add CA certificate.

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,6 +59,16 @@ return [
             'throw' => false,
             'report' => false,
         ],
+        'digitalocean' => [
+            'driver' => 's3',
+            'key' => env('DIGITALOCEAN_SPACES_KEY'),
+            'secret' => env('DIGITALOCEAN_SPACES_SECRET'),
+            'endpoint' => env('DIGITALOCEAN_SPACES_ENDPOINT'),
+            'region' => env('DIGITALOCEAN_SPACES_REGION'),
+            'bucket' => env('DIGITALOCEAN_SPACES_BUCKET'),
+            'visibility' => 'public', // Ensure public visibility
+
+            ],
 
     ],
 
